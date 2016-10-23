dc8e2e75dafb045e4318673b15f7b5d75308199b
lucasbf <lucasbf@gmail.com>
2016-10-23 09:51:09 -0200

    Aplicativo LetsDoIt

---
+++ LetsDoIt/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  LetsDoIt
+//
+//  Created by Lucas Baggio Figueira on 22/10/16.
+//  Copyright © 2016 Sci4Tech. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var title = ""
+    
+    var done = false
+    
+}
